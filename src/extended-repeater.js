4fef0b4dfcae30422af37da7467cd0629e561f9f
Stasya Geleysha <s.geleisha@gmail.com>
2021-02-05 13:54:05 +0300

    add task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,54 +1,53 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/*, options*/) {
- throw new CustomError('Not implemented');
+module.exports = function repeater(str, options) {
+ //throw new CustomError('Not implemented');
   // remove line with error and write your code here
   
-//  let curr = "";
-//  let  addition = "";
-//  if(typeof str != "string"){
-//    str.toString();
-//  }
-//  curr+=str;
+ let curr = "";
+ let  addition = "";
+ if(typeof str != "string"){
+   str.toString();
+ }
+ curr+=str;
  
-//   options.addition ? addition+=options.addition : addition+="";
+  options.addition ? addition+=options.addition : addition+="";
 
-//   let regExp;  
-//   if(options.additionSeparator){
-//     regExp = new RegExp(`\\${options.additionSeparator}$`);
-//     addition+=options.additionSeparator;
-//   }else{
-//     regExp = /\|$/;
-//     addition+="|";
-//   }
+  let regExp;  
+  if(options.additionSeparator){
+    //regExp = new RegExp(`\\${options.additionSeparator}$`);
+    addition+=options.additionSeparator;
+  }else{
+    regExp = /\|$/;
+    addition+="|";
+  }
   
-//   options.additionRepeatTimes ? addition = addition.repeat(+options.additionRepeatTimes) : addition; 
-//   addition = addition.replace(regExp, "");
-//   curr += addition;
+  options.additionRepeatTimes ? addition = addition.repeat(+options.additionRepeatTimes) : addition; 
+  if(regExp){
+    addition = addition.replace(regExp, "");
+  }else{
+    addition = addition.substr(0, options.additionSeparator.length-1);
+  }
   
-//   let regExPP;
-//   if(options.separator){
-//     if(/\w\d/gi.test(options.separator)){
-//       regExPP = new RegExp(`${options.separator}$`);
-//     }else{
-//       regExPP = new RegExp(`\${options.separator}$`);
-//     }   
-//     curr+=options.separator;    
-//   }else{
-//     regExPP= /\+$/;
-//     curr+="+";
-//   }
+  curr += addition;
+  
 
-//   options.repeatTimes ? curr = curr.repeat(+options.repeatTimes) : curr;
+  if(options.separator){     
+    curr+=options.separator;    
+  }else{   
+    curr+="+";
+  }
+
+  options.repeatTimes ? curr = curr.repeat(+options.repeatTimes) : curr;
  
   
-//   if(options.separator){
-//     let del = options.separator.length;
-//     curr = curr.substr(0, curr.length-del);
-//   }else{
-//     curr = curr.substr(0, curr.length-1);
-//   }
+  if(options.separator){
+    let del = options.separator.length;
+    curr = curr.substr(0, curr.length-del);
+  }else{
+    curr = curr.substr(0, curr.length-1);
+  }
 
-//   return curr;
+  return curr;
 };
   
